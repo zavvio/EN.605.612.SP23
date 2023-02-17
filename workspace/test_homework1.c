01c7e3f22c4417a5313ba3810f1268dc953a621f
Zavvio Mok <zavvio@gmail.com>
2023-02-16 18:16:05 -0800

    give some space

--- workspace/test_homework1.c
+++ workspace/test_homework1.c
@@ -11,9 +11,11 @@ int main(int argc, char **argv)
     int res = 0;
     message m;
     memset(&m, 0, sizeof(m));
+
     printf("%s:%d:%s - triggering system call with _syscall\n", __FILE__, __LINE__, __FUNCTION__);
     res = _syscall(PM_PROC_NR, PM_HOMEWORK, &m);
-    printf("%s:%d:%s - triggering system call from libc\n", __FILE__, __LINE__, __FUNCTION__);
+
+    printf("\n\n%s:%d:%s - triggering system call from libc\n", __FILE__, __LINE__, __FUNCTION__);
     res = homework1();
     return res;
 }
